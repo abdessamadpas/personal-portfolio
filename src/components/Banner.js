9758980a22bf84d89a5ea5b9c11adabf023e02b5
Abdessamad Pas <boughadoin@gmail.com>
2022-07-17 20:01:51 +0100

    bannner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm AbdessamadPas`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>   AALOUWAN Abdessamad is simply a Software Developer Student who enjoys spending countless hours coding.
+                  <p>   AALOUWAN Abdessamad is simply a Software Developer Student who enjoys spending countless hours coding .
                   .</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
